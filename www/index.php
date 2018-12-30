316e76333b26f74804bcd92f85e4f8d3e7057e3f
CreeperFace <honzaxp01@gmail.com>
2018-12-30 17:50:34 +0100

    idk

--- www/index.php
+++ www/index.php
@@ -4,10 +4,17 @@ $container = require __DIR__ . '/../app/bootstrap.php';
 
 \Tracy\Debugger::enable();
 
-$configurator->setDebugMode(true);
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
+
+$configurator->createRobotLoader()
+    ->addDirectory(__DIR__)
+    ->register();
+
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+//$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+$configurator->setDebugMode(TRUE);
 
 $container->getByType(Nette\Application\Application::class)
     ->run();
